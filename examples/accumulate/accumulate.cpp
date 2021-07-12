c043c9075400bc183c15bf26e057856590f16990
Scott Frees <scott.frees@gmail.com>
2021-07-12 14:32:45 -0400

    updated for node 16 compilation

--- examples/accumulate/accumulate.cpp
+++ examples/accumulate/accumulate.cpp
@@ -8,54 +8,61 @@
 
 using namespace std;
 
-class Accumulate : public StreamingWorker {
-  public:
-    Accumulate(Callback *data
-      , Callback *complete
-      , Callback *error_callback, 
-      v8::Local<v8::Object> & options) : StreamingWorker(data, complete, error_callback){
+class Accumulate : public StreamingWorker
+{
+public:
+  Accumulate(Callback *data, Callback *complete, Callback *error_callback,
+             v8::Local<v8::Object> &options) : StreamingWorker(data, complete, error_callback)
+  {
 
-        sum = 0;
-        filter = "";
-        if (options->IsObject() ) {
-          v8::Local<v8::Value> filter_ = options->Get(New<v8::String>("filter").ToLocalChecked());
-          if ( filter_->IsString() ) {
-            v8::String::Utf8Value s(filter_);
-            filter = *s;
-          }
-        }
+    sum = 0;
+    filter = "";
+    if (options->IsObject())
+    {
+      v8::Local<v8::Value> filter_ = Nan::Get(options, New<v8::String>("filter").ToLocalChecked()).ToLocalChecked(); //->Get(New<v8::String>("filter").ToLocalChecked());
+      if (filter_->IsString())
+      {
+        filter = *Nan::Utf8String(filter_);
+      }
     }
-    ~Accumulate(){}
-    
-    bool filter_by_name(string name) {
-      return ( filter.empty() || name == filter);
-    }
+  }
+  ~Accumulate() {}
 
-    void Execute (const AsyncProgressWorker::ExecutionProgress& progress) {
-      int value ;
-      do {
-        Message m = fromNode.read();
-        value = std::stoi(m.data);
-        if ( filter_by_name(m.name) || value <= 0) {
-          if ( value > 0 ){
-            sum += value;
-          }
-          else {
-            Message tosend("sum", std::to_string(sum));
-            writeToNode(progress, tosend);
-          }
+  bool filter_by_name(string name)
+  {
+    return (filter.empty() || name == filter);
+  }
+
+  void Execute(const AsyncProgressWorker::ExecutionProgress &progress)
+  {
+    int value;
+    do
+    {
+      Message m = fromNode.read();
+      value = std::stoi(m.data);
+      if (filter_by_name(m.name) || value <= 0)
+      {
+        if (value > 0)
+        {
+          sum += value;
         }
-      } while (value > 0);
-    }
-  private:
-    int sum;
-    string filter;
+        else
+        {
+          Message tosend("sum", std::to_string(sum));
+          writeToNode(progress, tosend);
+        }
+      }
+    } while (value > 0);
+  }
+
+private:
+  int sum;
+  string filter;
 };
 
-StreamingWorker * create_worker(Callback *data
-    , Callback *complete
-    , Callback *error_callback, v8::Local<v8::Object> & options) {
- return new Accumulate(data, complete, error_callback, options);
+StreamingWorker *create_worker(Callback *data, Callback *complete, Callback *error_callback, v8::Local<v8::Object> &options)
+{
+  return new Accumulate(data, complete, error_callback, options);
 }
 
 NODE_MODULE(accumulate, StreamWorkerWrapper::Init)
